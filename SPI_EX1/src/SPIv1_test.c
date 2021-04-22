7735c004f5d9e84ee9ee3a8e1f0f30306dcbec50
root <root@beaglebone.localdomain>
2021-04-22 08:35:40 +0000

    add code for ex1

---
+++ SPI_EX1/src/SPIv1_test.c
@@ -0,0 +1,58 @@
+/****************************************************************************/
+/*                                                                          */
+/* Structure of programs controlling CC1200                                 */
+/*                                                                          */
+/****************************************************************************/
+ 
+
+#include <stdio.h>
+#include <SPIv1.h> // necessary, otherwise CC1200 prototype are not available
+
+#include "smartrf_CC1200.h" // import register settings
+// #include "smartrf_adr_CC1200.h" // import register addresses
+ 
+int main (void) {
+	int adr;
+	int val;
+
+  	// first initialize
+  	if(spi_init()){
+    		printf("ERROR: Initialization failed\n");
+    		return -1;
+	}
+	// do some register reading or writing, 
+	// performance commands and get status information
+	
+	// reset CC1200
+        cc1200_cmd(SRES);
+
+	// CC1200 is now in idle mode, registers have their default values
+  	// Reprogram the registers
+	for (int i=0; i<MAX_REG; i++) {
+  		cc1200_reg_write(RegSettings[i].adr,RegSettings[i].val);
+  	}
+	
+
+	// Programm the RF frequency
+  	for (int i=0; i<MAX_EXT_REG; i++) {
+		cc1200_reg_write(ExtRegSettings[i].adr, ExtRegSettings[i].val);
+	}
+ 	 // get status information
+  	cc1200_cmd(SNOP);
+  	printf("INFO: Status:%s\n", get_status_cc1200_str());
+	
+	adr = 0x01;
+  	// register read
+  	cc1200_reg_read(adr, &val);
+  	printf("INFO:read Adr:0x%x Val:0x%x\n", adr, val);
+
+	// read extended register
+  	adr = EXT_ADR | 0x0A;
+  	cc1200_reg_read(adr, &val);
+	
+	// shutdown SPI
+  	spi_shutdown();
+ 
+ 	 return 0;
+
+}
