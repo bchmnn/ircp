47b216410abdbe31890c4ebaddbc865c317b4f8c
tarik <tarik.moussa@gt-arc.com>
2021-06-25 10:16:44 +0200

    add paramater

--- src/main.c
+++ src/main.c
@@ -10,6 +10,8 @@
 #include <string.h>
 #include <signal.h>
 #include <pthread.h>
+#include <getopt.h>
+# include <unistd.h>
 
 #include <external/SPIv1.h>
 
@@ -30,6 +32,10 @@
 
 #define BUF_SIZE 2048
 
+#define RX 0
+#define TX (!RX)
+ 
+
 // https://stackoverflow.com/a/37631288/14661040
 volatile sig_atomic_t sigint_received = 0;
 
@@ -38,12 +44,7 @@ void sigint_handler(int s) {
 	sigint_received = 1;
 }
 
-int main (void) {
-
-	// prepare sigint_handler
-	struct sigaction act;
-	act.sa_handler = sigint_handler;
-	sigaction(SIGINT, &act, NULL);
+void program_chat(){
 
 	cc1200_init_reg(RegSettings, ExtRegSettings);
 
@@ -84,5 +85,118 @@ int main (void) {
 	rb_free(buf);
 	spi_shutdown();
 
-	return 0;
+	return ;
+
 }
+
+
+void program_rssi(char mode){
+	cc1200_init_reg(RegSettings, ExtRegSettings);
+
+	
+	if (mode == RX){
+		cc1200_cmd(SRX);
+
+		signed char rssi = 0;
+		while (true) {
+			rssi = (signed char) cc1200_reg_read(RSSI1, 0);
+			printf("INFO: CC1200: RSSI1: %d\n", (int) rssi);
+			
+			sleep(1);
+		}
+	}else{
+		cc1200_cmd(STX);
+		while (true) {
+			sleep(1);
+
+		}
+	}
+
+		spi_shutdown();
+	
+		return;
+}
+char check_param(char* optarg ){
+	char param = RX; 
+	
+	if (strcasecmp(optarg,"tx")){
+		param = TX;
+	}
+	if (strcasecmp(optarg,"rx")){
+		param = RX;
+	}
+	return param;
+}
+
+void programm_interfier(){}
+
+int main (int argc, char **argv) {
+
+
+
+	// prepare sigint_handler
+	struct sigaction act;
+	act.sa_handler = sigint_handler;
+	sigaction(SIGINT, &act, NULL);
+
+	static struct option long_options[] = 
+		{
+			{"rssi",required_argument, 0, 'r' },
+			{"interfere", no_argument, 0 , 'i' },
+			{"chat",no_argument,0,'c'},
+			{"help", no_argument,0, 'h'},
+			{0, 0, 0, 0} 
+		};
+
+	//program_rssi(boolen mode(rx/tx)) 
+	//program_chatt()
+	//programm_interfier()
+	//help
+
+
+	int c;
+
+	while(1){
+
+		 /* getopt_long stores the option index here. */
+    	int option_index = 0;
+
+    	c = getopt_long (argc, argv, "r:ich",
+                       long_options, &option_index);
+		
+		  /* Detect the end of the options. */
+    	if (c == -1){break;}
+		switch (c)
+		{
+		case 0:
+
+		case 'c' :
+			LINFO("executing chat program\n");
+			program_chat();
+			break;
+		case 'r':
+			LINFO("executing rssi program\n");
+			program_rssi(check_param(optarg));
+			break;
+		
+		case 'i':
+			LINFO("executing interfere program\n");
+			break;
+		
+		case 'h':
+			printf("sage: <program> [OPTION]... OPTION -c, --chat main chat program -i, --interfere interfere program -r, --rssi=MODE meassure (MODE=rx) or send (MODE=tx) rssi -h, --help print this screen \n");
+			break;
+
+		default:
+			LINFO("executing chat program\n");
+			program_chat();
+			break;
+		}
+        
+	}
+
+
+	spi_shutdown();
+	return 0;
+
+	}
